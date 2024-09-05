10a4abb82d487e4b07bea64f60f4230da062176d
segfault0x61 <torresj1997@outlook.com>
2024-09-05 15:56:10 -0500

    feat: 🎸 started creating form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,20 @@ import { reducers } from './store/reducers';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MarkdownModule } from 'ngx-markdown';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MdPreviewComponent } from './components/md-preview/md-preview.component';
 import { BackgroundComponent } from './components/background/background.component';
+import { TechPickerComponent } from './components/tech-picker/tech-picker.component';
 
 @NgModule({
-  declarations: [AppComponent, MdPreviewComponent, BackgroundComponent],
+  declarations: [
+    AppComponent,
+    MdPreviewComponent,
+    BackgroundComponent,
+    TechPickerComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,6 +30,7 @@ import { BackgroundComponent } from './components/background/background.componen
       logOnly: false, // Restrict extension to log-only mode
     }),
     MarkdownModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
