b13fa93184c531f9bc0c530b5c65d0e4ec55e4a3
segfault0x61 <torresj1997@outlook.com>
2024-09-09 14:26:01 -0500

    feat: 🎸 value input support for features, and multifields

--- src/app/components/multi-field/multi-field.component.ts
+++ src/app/components/multi-field/multi-field.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 import { UtilsService } from '../../services/utils.service';
+import { FeatureOptions } from '../../interfaces/feature-options.interface';
 
 @Component({
   selector: 'app-multi-field',
@@ -20,15 +21,34 @@ export class MultiFieldComponent {
 
   constructor(private utilsService: UtilsService) {}
 
+  @Input()
+  set value(values: any[] | null) {
+    if (!values) return;
+
+    this.entries = [];
+    values.forEach((value) => {
+      console.log(typeof value);
+      if (typeof value === 'string') {
+        this.entries.push(this.generateFields([value]));
+      } else if (typeof value === 'object') {
+        Object.keys(value).forEach((key) => {
+          this.entries.push(this.generateFields([value[key], value[key]]));
+        });
+      }
+    });
+
+    console.log(this.entries);
+  }
+
   addEntry() {
     this.entries.push(this.generateFields());
   }
 
-  generateFields() {
+  generateFields(values: string[] = []) {
     const fields = [];
 
-    for (const field of this.fields) {
-      fields.push('');
+    for (let i = 0; i < this.fields.length; i++) {
+      fields.push(values[i] ?? '');
     }
     return { id: this.utilsService.guid(), name: fields };
   }
