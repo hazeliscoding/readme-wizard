d23f4be3680f6db8ae21308a4681e1592b3629ea
segfault0x61 <torresj1997@outlook.com>
2024-09-11 07:31:32 -0500

    feat: 🎸 add ui fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       height: {
         "100vh-h-7": "calc(100vh - 12rem)",
+      },
+      fontFamily: {
+        writter: ["Writter", ...defaultTheme.fontFamily.sans],
+        agrandir: ["Agrandir", ...defaultTheme.fontFamily.sans],
+        tn: ["TN", ...defaultTheme.fontFamily.sans],
       },
     },
   },
